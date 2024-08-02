4d6affaf765221483f1dc0f655589f2bb1122816
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-02 17:35:14 -0500

    M - 10:23 AM - moved all of my files out of my folder for testing purposes

--- non-interactive.c
+++ non-interactive.c
@@ -20,22 +20,30 @@ int non_interactive(void)
 		buf = NULL;
 		exit(EXIT_FAILURE);
 	}
-
-	args = get_tokens(buf, sep); /*makes an array of args from the string buf from getline*/
-	path = path_get(environ); /*gets the path from the environment*/
-	path_args = get_tokens(path, sep); /*makes an array of args from the path returned from the above function*/
-	temp = check_build(args[0], path_args); /*gets a workable path and puts it in postion 0 of the args array*/
-	if (temp == NULL)
+	if (strncmp("exit", buf, 4) == 0)
 	{
-		free_all(&args, &path_args, &buf, &temp);
-		perror("Cannot find command\n");
-		exit(EXIT_FAILURE);
+		free(buf);
+		buf = NULL;
+		exit(EXIT_SUCCESS);
 	}
 	else
 	{
-		args[0] = strdup(temp);
-		function_call(args, environ);
+		args = get_tokens(buf, sep); /*makes an array of args from the string buf from getline*/
+		path = path_get(environ); /*gets the path from the environment*/
+		path_args = get_tokens(path, sep); /*makes an array of args from the path returned from the above function*/
+		temp = check_build(args[0], path_args); /*gets a workable path and puts it in postion 0 of the args array*/
+		if (temp == NULL)
+		{
+			free_all(&args, &path_args, &buf, &temp);
+			perror("Cannot find command\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			args[0] = strdup(temp);
+			function_call(args, environ);
+		}
+		free_all(&args, &path_args, &buf, &temp);
 	}
-	free_all(&args, &path_args, &buf, &temp);
 	return (0);
 }
